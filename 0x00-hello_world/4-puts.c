e1ac33fce97ce9fd324a883fbadcd51df9a71b92
Hamdavilla <hamdavilla@gmail.com>
2023-03-16 21:52:46 -0700

    4-puts.c

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+ * main- serves as the starting point for program execution
+ *
+ * Return: ends the execution of a function, and returns control to the calling function 0
+ */
+
+int main(void)
+{
+	puts("\"Programming is like building a multilingual puzzle");
+	return (0);
+}
