ec85a956897adf4963207f10b31681cae2ef5daf
Hamdavilla <hamdavilla@gmail.com>
2023-03-20 15:51:34 -0700

    4-puts.c

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,7 @@
 #include <stdio.h>
 
 /**
- * main- serves as the starting point for program execution
- *
+ * main- serves as the starting point for program execution*
  * Return: ends the execution of a function, and returns control to the calling function 0
  */
 int main(void)
